64975b386f7db9391545e40a889c35d0a9c38ce2
Refactor-Hound <peterjayshepherd@gmail.com>
2026-02-03 23:12:33 -0500

    unit tests and main script

---
+++ src/test_textnode.py
@@ -0,0 +1,60 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_with_url(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
+        node2 = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_different_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_different_text_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_different_url(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
+        node2 = TextNode("This is a link", TextType.LINK, "https://www.google.com")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url_none(self):
+        node = TextNode("This is a text node", TextType.TEXT, None)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_one_url_none(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
+        node2 = TextNode("This is a link", TextType.LINK, None)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        expected = "TextNode(This is a text node, text, https://www.boot.dev)"
+        self.assertEqual(repr(node), expected)
+
+if __name__ == "__main__":
+    unittest.main()
